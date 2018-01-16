8b22b7f7c1eee7acd32154934fb1d0335303e9b1
CCInCharge <charles.l.chen.clc@gmail.com>
2018-01-15 22:59:20 -0800

    Remove type in Consumer

--- consumers/NewsConsumer.py
+++ consumers/NewsConsumer.py
@@ -1,16 +1,11 @@
 from kafka import KafkaConsumer
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
+import json
 
 # To consume latest messages and auto-commit offsets
-bootstrap_servers = [i for i in
-            os.environ.get("KAFKA_BOOTSTRAP_SERVERS").split(" ")]
 consumer = KafkaConsumer('twitter-topic',
-                         bootstrap_servers=bootstrap_servers,
+                         bootstrap_servers=['localhost:9092'],
                          auto_offset_reset='latest', enable_auto_commit=False,
-                         value_deserializer=lambda m: m)
+                         value_deserializer=lambda m: json.loads(m))
 
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
