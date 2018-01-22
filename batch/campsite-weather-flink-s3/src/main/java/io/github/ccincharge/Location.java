439bc62a347b45ebbb4b75730b16351eea0455bc
CCInCharge <charles.l.chen.clc@gmail.com>
2018-01-22 02:45:28 +0000

    Remove vestigial Flink code

--- batch/campsite-weather-flink-s3/src/main/java/io/github/ccincharge/Location.java
+++
@@ -1,9 +0,0 @@
-package io.github.ccincharge;
-
-import java.io.Serializable;
-
-public class Location implements Serializable {
-    public Double lat;
-    public Double lon;
-    public Location() {}
-}
