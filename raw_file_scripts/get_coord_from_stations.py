70e91c686f4cd06497daadb08b4689d117aceb26
CCInCharge <charles.l.chen.clc@gmail.com>
2018-02-01 15:09:21 -0800

    Change title of HTML template

--- raw_file_scripts/get_coord_from_stations.py
+++ raw_file_scripts/get_coord_from_stations.py
@@ -1,20 +1,21 @@
 import json
 
-output_obj = {}
-with open("isd-history.txt") as f:
-    for line in f:
-        lat = line[57:64]
-        lon = line[65:73]
-        USAF = line[0:6]
-        WBAN = line[7:12]
-        try:
-            float(lat)
-            float(lon)
-            int(USAF)
-            int(WBAN)
-        except:
-            continue
-        output_obj[str(USAF) + "|" + str(WBAN)] = {"lat": lat, "lon": lon}
+if __name__ == "__main__":
+    output_obj = {}
+    with open("isd-history.txt") as f:
+        for line in f:
+            lat = line[57:64]
+            lon = line[65:73]
+            USAF = line[0:6]
+            WBAN = line[7:12]
+            try:
+                float(lat)
+                float(lon)
+                int(USAF)
+                int(WBAN)
+            except:
+                continue
+            output_obj[str(USAF) + "|" + str(WBAN)] = {"lat": lat, "lon": lon}
 
-with open("stations_latlon.json", "w") as output:
-    output.write(json.dumps(output_obj))
+    with open("stations_latlon.json", "w") as output:
+        output.write(json.dumps(output_obj))
