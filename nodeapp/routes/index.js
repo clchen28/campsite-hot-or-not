6af9aa11e15b7d87fd0c577fa43b9e641cfe09dc
CCInCharge <charles.l.chen.clc@gmail.com>
2018-02-04 23:27:18 -0800

    Add redirect to Google Slides

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/slides', function(req, res, next) {
+  res.redirect('https://docs.google.com/presentation/d/1gGlgV1YE199ZMC27SL9aNVAoPzZZmusbKoBV8IPybpw/edit?usp=sharing');
+});
+
 module.exports = router;
